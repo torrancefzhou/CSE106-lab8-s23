e4e18efcae27230ce9bf2f22b53a3eb96d99b491
CW 2 <cw2@Wandas-MacBook.attlocal.net>
2023-04-07 01:37:55 -0700

    makes displaying db to html work

---
+++ static/script.js
@@ -0,0 +1,27 @@
+function printToConsole() {
+    username = document.getElementById("username").value
+    password = document.getElementById("password").value
+
+    console.log(username + " " + password)
+}
+
+function getGrades() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "http://127.0.0.1:5000/home");
+    xhttp.onload = function() {
+      var data = JSON.parse(this.responseText);
+      var table = "<table border='1'>";
+      table += "<thead><tr><th>Name</th><th>Instructor</th><th>Time</th><th>Current Enrollment</th></tr><th>Max Enrollment</th></tr></thead>";
+      table += "<tbody>";
+      for (var i = 0; i < data.length; i++) {
+        table += "<tr><td>" + data[i].name + "</td>";
+        table += "<td>" + data[i].instructor + "</td>";
+        table += "<td>" + data[i].time + "</td>";
+        table += "<td>" + data[i].currentEnrollment + "</td>";
+        table += "<td>" + data[i].maxEnrollment + "</td></tr>";
+      }
+      table += "</tbody></table>";
+      document.getElementById("demo").innerHTML = table;
+    };
+    xhttp.send();
+  }
